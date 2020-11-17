ee1ba5d6b42c9c14e203f48aee3a36737c786f59
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-18 02:43:38 +0600

    Add courses component
    add a new componet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TopNavigation from "./component/topNavigation/TopNavigation";
 import Analysis from "./component/Analysis/Analysis";
 import Summary from "./component/Summary/Summary";
 import RecentProjects from "./component/RecentProjects/RecentProjects";
+import Courses from "./component/Courses/Courses";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Analysis/>
       <Summary/>
       <RecentProjects/>
+      <Courses/>
     </div>
   );
 }
---
+++ src/component/Courses/Courses.js
@@ -0,0 +1,20 @@
+import React, {Component, Fragment} from 'react';
+import {Button, Col, Container, Row} from "react-bootstrap";
+import courseImg from "../../asset/image/courseImg.jpg"
+
+class Courses extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container className="text-center">
+                    <h2 className="serviceNameTitle">Our Courses</h2>
+                    <Row>
+                     
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default Courses;
