97d4ee05491b1b9aadd6ec0de9913f40e2918fdf
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-18 19:23:38 +0600

    intall and setting react-slick carousel
    For carousel slider intall react-slick rarousel API. and settings

--- src/component/ClientReview/ClientReview.js
+++ src/component/ClientReview/ClientReview.js
@@ -6,12 +6,51 @@ import {Container, Row,Col} from "react-bootstrap";
 
 class ClientReview extends Component {
     render() {
+        var settings = {
+            autoplay:true,
+            autoplaySpeed:3000,
+            dots: true,
+            infinite: false,
+            vertical:true,
+            verticalSwiping:true,
+            speed: 3000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        initialSlide: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+
+        
         return (
             <Fragment>
                 <Container>
                     <Row>
                         <Col lg={12} md={12} sm={12}>
-                            
+
                         </Col>
                     </Row>
                 </Container>
