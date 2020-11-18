0257fb633efee566b6aa008e81eba4fbab96d705
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-18 12:32:06 +0600

    Add container and make respossive
    add cotainer and make it's responsive

--- src/component/Courses/Courses.js
+++ src/component/Courses/Courses.js
@@ -9,8 +9,50 @@ class Courses extends Component {
                 <Container className="text-center">
                     <h2 className="serviceNameTitle">Our Courses</h2>
                     <Row>
-                     
+                        <Col lg={6} md={12} sm={12}>
+                           <Row>
+                               <Col lg={6} md={12} sm={12}><h4>Image</h4></Col>
+                               <Col lg={6} md={12} sm={12}>
+                                   <h4>Web Development</h4>
+                                   <p>I build native and cross platform mobile app for your business</p>
+                                   <button variant="primary">Details</button>
+                               </Col>
+                           </Row>
+                        </Col>
+                        <Col lg={6} md={12} sm={12}>
+                            <Row>
+                                <Col lg={6} md={12} sm={12}><h4>Image</h4></Col>
+                                <Col lg={6} md={12} sm={12}>
+                                    <h4>Web Development</h4>
+                                    <p>I build native and cross platform mobile app for your business</p>
+                                    <button variant="primary">Details</button>
+                                </Col>
+                            </Row>
+                        </Col>
                     </Row>
+                    <Row>
+                        <Col lg={6} md={12} sm={12}>
+                            <Row>
+                                <Col lg={6} md={12} sm={12}><h4>Image</h4></Col>
+                                <Col lg={6} md={12} sm={12}>
+                                    <h4>Web Development</h4>
+                                    <p>I build native and cross platform mobile app for your business</p>
+                                    <button variant="primary">Details</button>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col lg={6} md={12} sm={12}>
+                            <Row>
+                                <Col lg={6} md={12} sm={12}><h4>Image</h4></Col>
+                                <Col lg={6} md={12} sm={12}>
+                                    <h4>Web Development</h4>
+                                    <p>I build native and cross platform mobile app for your business</p>
+                                    <button variant="primary">Details</button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
                 </Container>
             </Fragment>
         );
