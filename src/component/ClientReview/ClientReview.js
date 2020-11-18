2b8322ad8a2fb3b1973e1029729432a2fd46f80f
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-18 19:02:41 +0600

    add ClientReview components

---
+++ src/component/ClientReview/ClientReview.js
@@ -0,0 +1,23 @@
+import React, {Component, Fragment} from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import {Container, Row,Col} from "react-bootstrap";
+
+class ClientReview extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <Row>
+                        <Col lg={12} md={12} sm={12}>
+                            
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default ClientReview;
