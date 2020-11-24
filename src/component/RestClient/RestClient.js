de11070f487672a068e3483110ef491a019dfd47
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-24 22:42:04 +0600

    Create RestClient.js

---
+++ src/component/RestClient/RestClient.js
@@ -0,0 +1,14 @@
+
+import axios from 'axios';
+
+
+ class RestClient{
+    static getRequest(getUrl){
+        axios.get(getUrl).then(response=>{
+          return   response.data;
+        }).catch(error=>{
+            return null;
+        })
+    }
+}
+export default RestClient;
