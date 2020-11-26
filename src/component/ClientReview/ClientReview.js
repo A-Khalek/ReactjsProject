e72763ee6cf6485327d418893d4b1de81ffb0de0
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-26 23:00:59 +0600

    Update ClientReview.js

--- src/component/ClientReview/ClientReview.js
+++ src/component/ClientReview/ClientReview.js
@@ -4,8 +4,25 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import {Container, Row,Col} from "react-bootstrap";
 import circleImg from '../../asset/image/circleImg.png'
+import RestClient from "../RestClient/RestClient";
+import AppURL from "../../RestAPI/AppURL";
+import {Link} from "react-router-dom";
 
 class ClientReview extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            myData:[]
+        }
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppURL.ClientReview).then(result=>{
+            this.setState({myData:result})
+        })
+    }
+
     render() {
         var settings = {
             autoplay:true,
@@ -46,37 +63,27 @@ class ClientReview extends Component {
         };
 
 
+        const myList = this.state.myData;
+        const myView = myList.map(myList=> {
+
+            return  <Row>
+                        <Col lg={12} md={12} sm={12}>
+                            <div>
+                                <img className="circleImg" src={myList.review_img}></img>
+                                <h4 className="clientName">{myList.review_title}</h4>
+                                <p className="clientDesc">{myList.review_desc}</p>
+                            </div>
+                        </Col>
+                    </Row>
+        })
+
         return (
             <Fragment>
                 <Container className="text-center">
                     <h2 className="serviceNameTitle">CLIENT SAYS</h2>
-                    <Row>
-                        <Col lg={12} md={12} sm={12}>
-                            <Slider {...settings}>
-                                <div>
-                                    <img className="circleImg" src={circleImg}></img>
-                                    <h4 className="clientName">Md. Abdul Khalek</h4>
-                                    <p className="clientDesc">First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i start coding. </p>
-                                </div>
-
-                                <div>
-                                    <img className="circleImg" src={circleImg}></img>
-                                    <h4 className="clientName">Md. Abdul Khalek</h4>
-                                    <p className="clientDesc">First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i start coding. </p>
-                                </div>
-                                <div>
-                                    <img className="circleImg" src={circleImg}></img>
-                                    <h4 className="clientName">Md. Abdul Khalek</h4>
-                                    <p className="clientDesc">First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i start coding. </p>
-                                </div>
-                                <div>
-                                    <img className="circleImg" src={circleImg}></img>
-                                    <h4 className="clientName">Md. Abdul Khalek</h4>
-                                    <p className="clientDesc">First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i start coding. </p>
-                                </div>
-                            </Slider>
-                        </Col>
-                    </Row>
+                        <Slider {...settings}>
+                            {myView}
+                        </Slider>
                 </Container>
             </Fragment>
         );
