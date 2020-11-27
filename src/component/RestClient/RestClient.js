8b86f09dbd9e5cd1d7b67a972e66d6b4bb1db396
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-27 23:55:03 +0600

    Update RestClient.js

--- src/component/RestClient/RestClient.js
+++ src/component/RestClient/RestClient.js
@@ -10,12 +10,11 @@ export default class RestClient  {
         })
     }
 
-    static PostRequest(postUrl,postJson){
+    static PostRequest=(postUrl,postJson)=>{
         let config={
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }
-
-        return  axios.get(postUrl,postJson,config).then(response=>{
+        return axios.get(postUrl,postJson,config).then(response=>{
             return  response.data;
         }).catch(error=>{
             return error;
