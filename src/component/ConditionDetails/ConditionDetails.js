346dfbb757f95116d3b9dcc1adf3557ce7f39f99
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-27 10:19:12 +0600

    Update ConditionDetails.js
    Terms and conditon

--- src/component/ConditionDetails/ConditionDetails.js
+++ src/component/ConditionDetails/ConditionDetails.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import RestClient from "../RestClient/RestClient";
+import AppURL from "../../RestAPI/AppURL";
 
 class ConditionDetails extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            condition:""
+        }
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppURL.information).then(result=>{
+            this.setState({condition:result[0]['terms']})
+        })
+    }
+
     render() {
         return (
             <Container className="pt-5">
                 <Row>
                     <Col lg={12} md={12} sm={12}>
-                        <p>
-                            BY ACCESSING OR USING THIS SITE YOU REPRESENT THAT YOU HAVE THE FULL AUTHORITY TO ACT TO BIND YOURSELF, ANY THIRD PARTY, COMPANY, OR LEGAL ENTITY, AND THAT YOUR USE AND/OR INTERACTION, AS WELL AS CONTINUING TO USE OR INTERACT, WITH THE SITE CONSTITUTES YOUR HAVING READ AND AGREED TO THESE TERMS OF USE AS WELL AS OTHER AGREEMENTS THAT WE MAY POST ON THE SITE.
-                            BY ACCESSING OR USING THIS SITE YOU REPRESENT THAT YOU HAVE THE FULL AUTHORITY TO ACT TO BIND YOURSELF, ANY THIRD PARTY, COMPANY, OR LEGAL ENTITY, AND THAT YOUR USE AND/OR INTERACTION, AS WELL AS CONTINUING TO USE OR INTERACT, WITH THE SITE CONSTITUTES YOUR HAVING READ AND AGREED TO THESE TERMS OF USE AS WELL AS OTHER AGREEMENTS THAT WE MAY POST ON THE SITE
-                        </p>
+                       <p>
+                           {this.state.condition}
+                       </p>
                     </Col>
                 </Row>
             </Container>
