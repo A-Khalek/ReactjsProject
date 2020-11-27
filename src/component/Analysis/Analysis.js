8f7753de4e80940d8e881134920e8686c8b2d518
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-27 09:22:29 +0600

    Install React-html-parser
    intall react-html-parser and apply it in technology section

--- src/component/Analysis/Analysis.js
+++ src/component/Analysis/Analysis.js
@@ -4,6 +4,7 @@ import {Bar, ResponsiveContainer, XAxis,Tooltip} from "recharts";
 import BarChart from "recharts/lib/chart/BarChart";
 import RestClient from "../RestClient/RestClient";
 import AppURL from "../../RestAPI/AppURL";
+import ReactHtmlParser from 'react-html-parser';
 
 class Analysis extends Component {
     
@@ -44,7 +45,7 @@ class Analysis extends Component {
                           </Col>
                           <Col lg={6} md={12} sm={12}>
                               <div>
-                                <p className="desc text-justify">{this.state.techDesc}</p>
+                                <p className="desc text-justify">{ ReactHtmlParser(this.state.techDesc) }</p>
                               </div>
                           </Col>
                       </Row>
