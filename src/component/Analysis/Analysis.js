dbf761874cac6b419a52e49d87b789c556047436
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-27 09:16:09 +0600

    Update Analysis.js
    Technology section updated

--- src/component/Analysis/Analysis.js
+++ src/component/Analysis/Analysis.js
@@ -10,13 +10,17 @@ class Analysis extends Component {
     constructor() {
         super();
         this.state={
-            data:[]
+            data:[],
+            techDesc:" "
         }
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppURL.ChartData).then(result=>{
             this.setState({data:result})
+        })
+        RestClient.GetRequest(AppURL.tectDesc).then(result=>{
+            this.setState({techDesc:result[0]['technology_desc']})
         })
     }
 
@@ -28,23 +32,19 @@ class Analysis extends Component {
                   <Container className="text-center">
                       <h1 className="serviceNameTitle">Technology Used</h1>
                       <Row>
-                          <Col style={{width:'100%', height:'300px'}} lg={6} md={12} sm={12}>
+                          <Col style={{width:'100%', height:'300px'}} lg={6} md={12} sm={12}>f
                               <ResponsiveContainer>
                                   <BarChart width={100} height={300} data={this.state.data} >
                                       <XAxis dataKey="technology"/>
                                       <Tooltip/>
-                                      <Bar dataKey="project" fill={blue}>
-
-                                      </Bar>
+                                      <Bar dataKey="project" fill={blue}></Bar>
                                   </BarChart>
                               </ResponsiveContainer>
 
                           </Col>
                           <Col lg={6} md={12} sm={12}>
                               <div>
-                                <p className="desc text-justify">To build native android apps i use Java as well as kotline mainly. React JS is used for cross platform mobile application. I use MySQL database for relational database system. To build NoSQL application i use MongoDB.
-                                   <br/><br/> Firebase database system is used where it is necessary to provide realtime data flow facilities. I always build dynamic application. Admin panel is the heart of such kinds of application. I always try to provide sufficient features in admin panel to dominate application.
-                                    <br/><br/> According to client demand I use PHP OOP, CodeIgniter and Laravel to build admin panel section. Application security is a big deal for commercial application.I always ensure security portion of my application, moreover i build a security alert system, to notify admin when his system at risk.</p>
+                                <p className="desc text-justify">{this.state.techDesc}</p>
                               </div>
                           </Col>
                       </Row>
