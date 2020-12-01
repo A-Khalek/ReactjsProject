973d729f621dadf6fa8452978fbad2e0b5beb33a
A-Khalek <khalek.raju.bd@gmail.com>
2020-12-01 20:13:17 +0600

    Update PrivacyPolicy.js

--- src/component/PrivacyPolicy/PrivacyPolicy.js
+++ src/component/PrivacyPolicy/PrivacyPolicy.js
@@ -3,34 +3,54 @@ import {Col, Container, Row} from "react-bootstrap";
 import RestClient from "../RestClient/RestClient";
 import AppURL from "../../RestAPI/AppURL";
 import ReactHtmlParser from 'react-html-parser';
+import Loading from "../Loading/Loading";
+import WentWrong from "../WentWrong/WentWrong";
 
 
 class PrivacyPolicy extends Component {
     constructor() {
         super();
         this.state={
-            policy:"..."
+            policy:"",
+            loading:true,
+            error:false
         }
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppURL.information).then(result=>{
-            this.setState({policy:result[0]['privacy']})
+            if (result==null){
+                this.setState({error:true,loading:false})
+            }
+            else {
+                this.setState({policy:result[0]['privacy']})
+            }
+        }).catch(error=>{
+            this.setState({error:true,loading:false})
         })
     }
 
     render() {
-        return (
-            <Fragment>
-                <Container className="pt-5">
-                    <Row>
-                        <Col lg={12} md={12} sm={12} className="courseDesc">
-                            {ReactHtmlParser(this.state.policy)}
-                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+        if (this.state.loading==false && this.state.error== false){
+            return <Loading/>
+        }
+        else if (this.state.loading == true){
+            return (
+                <Fragment>
+                    <Container className="pt-5">
+                        <Row>
+                            <Col lg={12} md={12} sm={12} className="courseDesc">
+                                {ReactHtmlParser(this.state.policy)}
+                            </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+            );
+        }
+        else if (this.state.error==true){
+            return <WentWrong/>
+        }
+
     }
 }
 
